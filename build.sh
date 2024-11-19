aea56424d148a022999bf74000e8e7a1568e6bbc
kodachi77 <12465849+kodachi77@users.noreply.github.com>
2024-11-18 22:21:40 -0500

    improved build script

--- build.sh
+++ build.sh
@@ -9,3 +9,8 @@ mkdir -p ${FINN_BUILD_DIR}
 
 cd ${FINN_ROOT}
 ./run-docker.sh build_custom ${SCRIPT_DIR}
+
+cd "${SCRIPT_DIR}/output_sfc_1w1a_fpga/sim"
+vivado -mode batch -source make_sim_proj.tcl
+cd "${SCRIPT_DIR}"
+
